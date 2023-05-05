a510f8b5eac84187fdb1df519defab0a24260d54
Rakesh Baruah <rbaruah@gmail.com>
2023-05-05 11:42:01 -0500

    create random destination API and configure in redux store

---
+++ src/api/randomDestinationApi.jsx
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const randomDestinationApi = createApi({
+  reducerPath: "apiRandomDestination",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://random-data-api.com/api/v2/" }),
+  endpoints: (builder) => ({
+    // QUERY -> GET
+    getRandomDestination: builder.query({
+      query: () => ({
+        url: "addresses",
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const { getRandomDestination } = randomDestinationApi;
