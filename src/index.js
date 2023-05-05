391c036f420584aa9474f68ec0a3850133ac6dbd
Rakesh Baruah <rbaruah@gmail.com>
2023-05-05 11:37:13 -0500

    integrate rtkQuery w/ redux store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./layout/Header";
 import DestinationIndex from "./components/DestinationIndex";
-import { destinationApi } from "./api/destinationApi";
+
 import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider api={destinationApi}>
+    <Provider store={store}>
       <Header />
       <DestinationIndex />
     </Provider>
